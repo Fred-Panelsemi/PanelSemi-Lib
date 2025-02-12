7fa3d47aba0337f545a66ac321ca13059b4a59d1
峰瑜 張 <fred.chang@panelsemi.com>
2025-02-12 09:31:38 +0800

    加入專案檔案。

---
+++ PanelSemiLib/ViewModel/MainProcess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PanelSemiLib.ViewModel
+{
+    internal class MainProcess
+    {
+        public string PanelSemiLib_Version { get; }
+
+        //Panelsemi_ColorAdjustment
+        public ICommand CallColor { get; set; }
+        public MainProcess() 
+        {
+            /* 撈出版本號　=> 由AssemblyInfo.cs中設定 */
+            var asm = Assembly.GetExecutingAssembly();
+            PanelSemiLib_Version = $"v{asm.GetName().Version}";
+
+            CallColor = new RelayCommand(CallColor_Action);
+            
+        }
+
+        private void CallColor_Action()
+        {
+
+        }
+    }
+}
